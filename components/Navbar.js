86a49689b8f0d9e008260805058f9768759a6f08
Farah Nazihah <farahnazihah102@gmail.com>
2022-08-13 13:35:32 +0700

    Fix styling navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,7 @@ import { useColorMode, Flex, HStack, Spacer, Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 import styled from "@emotion/styled";
+import { Image, Box } from "@chakra-ui/react";
 
 const Navbar = () => {
   const { colorMode } = useColorMode({});
@@ -10,30 +11,44 @@ const Navbar = () => {
     position: sticky;
     z-index: 10;
     top: 0;
-    background: ${colorMode === "light" ? "#F8F8F8" : "#1A202C"};
   `;
 
   return (
-    <Navbar width="100%" as="nav" py={2} mb={[0, 0, 8]} px={["5px", "5rem"]}>
-      <HStack spacing="2rem">
-        <NextLink href="/" passHref>
-          <Button as="a" _hover={{ bgColor: "primary", color: "white" }}>
-            Shipment
-          </Button>
-        </NextLink>
-        <NextLink href="/" passHref>
-          <Button as="a" _hover={{ bgColor: "primary", color: "white" }}>
-            Truck
-          </Button>
-        </NextLink>
-        <NextLink href="/" passHref>
-          <Button as="a" _hover={{ bgColor: "primary", color: "white" }}>
-            Driver
-          </Button>
-        </NextLink>
-      </HStack>
-      <Spacer />
-    </Navbar>
+    <Box borderBottomColor="primary" borderWidth="5px">
+      <Navbar width="100%" as="nav" py={2} px={["5px", "5rem"]}>
+        <Image src={"/logo.png"} alt="logo" width={"100px"} mr="5rem" />
+        <HStack spacing="2rem">
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              bg={"white"}
+              _hover={{ bgColor: "primary", color: "white" }}
+            >
+              Shipment
+            </Button>
+          </NextLink>
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              bg={"white"}
+              _hover={{ bgColor: "primary", color: "white" }}
+            >
+              Truck
+            </Button>
+          </NextLink>
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              bg={"white"}
+              _hover={{ bgColor: "primary", color: "white" }}
+            >
+              Driver
+            </Button>
+          </NextLink>
+        </HStack>
+        <Spacer />
+      </Navbar>
+    </Box>
   );
 };
 
